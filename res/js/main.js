e61928b162eb2285a61e4b7613fc23e4337d5304
honziktillu <till@spsmb.cz>
2023-10-20 09:02:25 +0200

    added click upgrade

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,24 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+const autoclickerUpgrade = document.getElementById("autoclickerUpgrade");
 
 let numberOfCookies = 0;
+let cookieIncreaseNumber = 1;
 
 cookie.onclick = () => {
-    numberOfCookies++;
+    numberOfCookies += cookieIncreaseNumber;
     //counter.innerHTML = "Cookies: " + numberOfCookies;
     counter.innerHTML = `Cookies: ${numberOfCookies}`;
+}
+
+clickUpgrade.onclick = () => {
+    if (numberOfCookies >= 50) {
+        //odecist cenu nakupu
+        //numberOfCookies = numberOfCookies - 50;
+        numberOfCookies -= 50;
+        counter.innerHTML = `Cookies: ${numberOfCookies}`;
+        //zvednout klikani o 1
+        cookieIncreaseNumber++;
+    }
 }
